c19a3ef14e15f494c22aa367d20d7ce1e77ba232
Manolo Carrasco <manolo@vaadin.com>
2016-05-13 22:52:48 +0200

    Simplify bower.json and bundle source file

--- gulpfile.js
+++ gulpfile.js
@@ -11,27 +11,23 @@ var htmlname = bundle + '.html';
 var minname = bundle + '.min.html';
 
 gulp.task('bootstrap', function() {
-  return gulp.src('bower_components/webcomponentsjs/webcomponents-lite.min.js')
-          .pipe(rename(jsname))
-          .pipe(insert.append(
-            'var path = document.querySelector(\'script[src$="' + jsname + '"]\').src.replace(/' + jsname + '$/,\'\');' +
-            'document.write(\'<link rel="import" href="\' + path + \'' + minname + '">\');'
-          ))
-          .pipe(gulp.dest('.'));
+    return gulp.src('bower_components/webcomponentsjs/webcomponents-lite.min.js')
+    .pipe(rename(jsname))
+    .pipe(insert.append(
+      'var path = document.querySelector(\'script[src$="' + jsname + '"]\').src.replace(/' + jsname + '$/,\'\');' +
+      'document.write(\'<link rel="import" href="\' + path + \'' + minname + '">\');'
+    ))
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('vulcanize', function () {
-	return gulp.src(htmlname)
-		.pipe(vulcanize({
-			abspath: '',
-			stripExcludes: false,
-      inlineScripts: true,
-      inlineCss: true,
-      // TODO: adding 'bower_components/iron-doc-viewer/iron-doc-viewer.html'
-      // does not works and prevents including certain things like polymer.html.
-      excludes: [
-      ]
-		}))
+    return gulp.src(htmlname)
+    .pipe(vulcanize({
+        abspath: '',
+        stripExcludes: false,
+        inlineScripts: true,
+        inlineCss: true
+    }))
     .pipe(htmlmin({
       collapseWhitespace: true,
       removeComments: true,
@@ -40,7 +36,7 @@ gulp.task('vulcanize', function () {
     }))
     .pipe(minifyInline())
     .pipe(rename(minname))
-		.pipe(gulp.dest('.'));
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('default', ['bootstrap', 'vulcanize'], function () {
